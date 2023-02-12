a3cfb6c9fa3eb3c142d7dcfb4721b9e50eb26294
jemraider21 <jemraider21@gmail.com>
2023-02-12 18:28:16 -0500

    Housing.py - 10/10

--- Utils/Models/housing.py
+++ Utils/Models/housing.py
@@ -1,17 +1,16 @@
 """Model for the Housing.csv file
 """
+from dataclasses import dataclass
 
 
+@dataclass
 class Housing:
     """Model statistics for a house
     """
-
-    def __init__(self, age: int, bedrooms: int, year_built: int,
-                 num_units: int, rooms: int, weight: float, utility: float) -> None:
-        self.age: int = age
-        self.bedrooms: int = bedrooms
-        self.year_build: int = year_built
-        self.num_units: int = num_units
-        self.rooms: int = rooms
-        self.weight: float = weight
-        self.utility: float = utility
+    age: int
+    bedrooms: int
+    year_build: int
+    num_units: int
+    rooms: int
+    weight: float
+    utility: float
